6fc644d33ee8995f3c6bedc8b2e614c633e305c8
GalaArITT <oljagalavizar@ittepic.edu.mx>
2018-11-26 14:46:05 -0700

    acomodar opciones en pantallas principales

--- siteSPPP/Controllers/AdministradorController.cs
+++ siteSPPP/Controllers/AdministradorController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PagedList;
+using System.IO;
 
 namespace siteSPPP.Controllers
 {
@@ -44,6 +45,275 @@ namespace siteSPPP.Controllers
             }
             return null;
         }
+        //MODULO DE NOTICIAS//
+
+        public ActionResult ListaNoticiasAdmin(string filtrarfech, string filtrado, string currentFilter, string busqueda, int? page)
+        {
+            //Asegurar que a esta vista solo entren aquellos usuarios con rol 2=Administrador 
+            int idUsuario = Convert.ToInt32(Session["IDUSUARIO"]);
+            var noticias = from s in db.NOTICIAS
+                           select s;
+            byte? rol = null;
+            //linea para validar que no entre a los controladores hasta que detecte una autenticación
+            if (idUsuario == 0)
+            {
+                Response.Redirect("~/Login/Iniciar");
+                rol = null;
+            }
+            //en caso de que si detecte asignar el valor de rol y dar seguridad
+            else
+            {
+                rol = db.USUARIO.Where(s => s.IDUSUARIO == idUsuario).FirstOrDefault().ROL;
+                if (rol != 2) // 2 = Administrador
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+                else
+                {
+                    if (busqueda != null)
+                    {
+                        page = 1;
+                    }
+                    else
+                    {
+                        busqueda = currentFilter;
+                    }
+                    //buscar por nombre de servidor 
+                    // busqueda = busqueda.ToString();
+                    if (!String.IsNullOrEmpty(busqueda))
+                    {
+                        noticias = noticias.Where(s => s.TITULO.Contains(busqueda) || s.CONTENIDO.Contains(busqueda));
+                        ViewBag.Currentfilter = busqueda;
+                    }
+                    //Filtrar por estatus
+                    if (!string.IsNullOrEmpty(filtrado))
+                    {
+                        filtrado = filtrado.ToString();
+                        noticias = noticias.Where(s => s.ESTATUS.ToString().Contains(filtrado));
+                        ViewBag.filtrado = filtrado;
+                    }
+                    //Filtrar por fechas
+                    if (!string.IsNullOrEmpty(filtrarfech))
+                    {
+                        DateTime filtrarfech1 = Convert.ToDateTime(filtrarfech).Date;
+                        noticias = noticias.Where(s => s.FECHAPUBLIC == filtrarfech1);
+                        ViewBag.filtrarfech = filtrarfech;
+                    }
+                    //Mostrar una lista de noticias desde la tabla objeto NOTICIAS que hace referencia a NOTICIAS_SEPLA
+                    int pageSize = 5;
+                    int pageNumber = (page ?? 1);
+
+                    return View(noticias.OrderBy(x => x.FECHAPUBLIC).ToPagedList(pageNumber, pageSize));
+                }
+            }
+            return null;
+        }
+        // GET: NoticiasTesting/Edit/5
+        public ActionResult EditarNoticiasAdmin(int? id)
+        {
+            //Asegurar que a esta vista solo entren aquellos usuarios con rol 1=Capturista 
+            int idUsuario = Convert.ToInt32(Session["IDUSUARIO"]);
+            byte? rol = null;
+            //linea para validar que no entre a los controladores hasta que detecte una autenticación
+            if (idUsuario == 0)
+            {
+                Response.Redirect("~/Login/Iniciar");
+                rol = null;
+            }
+            //en caso de que si detecte asignar el valor de rol y dar seguridad
+            else
+            {
+                rol = db.USUARIO.Where(s => s.IDUSUARIO == idUsuario).FirstOrDefault().ROL;
+                if (rol != 2) // 2 = Administrador
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+                else
+                {
+                    if (id == null)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+                    NOTICIAS_SEPLAN nOTICIAS_SEPLAN = db.NOTICIAS.Find(id);
+                    if (nOTICIAS_SEPLAN == null)
+                    {
+                        return HttpNotFound();
+                    }
+                    ViewBag.IDNOTICIA = new SelectList(db.FOTOS, "IDNOTICIA", "IDNOTICIA", nOTICIAS_SEPLAN.IDNOTICIA);
+                    return View(nOTICIAS_SEPLAN);
+                }
+            }
+            return null;
+        }
+
+        // POST: Editar Noticas
+        [HttpPost]
+        [ValidateAntiForgeryToken, ValidateInput(false)]
+        public ActionResult EditarNoticiasAdmin([Bind(Include = "IDNOTICIA,IDUSUARIO,TITULO,CONTENIDO,FECHACAPTURA,FECHAPUBLIC,LUGAR,PRIORIDAD,ESTATUS")] NOTICIAS_SEPLAN Noticias_Seplan)
+        {
+            //hay que agregar todos los campos en el bind y los idnoticia, idusuario, estatus, fechacaptura hay que colocarlos dentro de un 
+            //hidden sino este desgraciado segmento
+            if (ModelState.IsValid)
+            {
+                db.Entry(Noticias_Seplan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ListaNoticiasAdmin");
+            }
+            ViewBag.IDNOTICIA = new SelectList(db.FOTOS, "IDNOTICIA", "IDNOTICIA", Noticias_Seplan.IDNOTICIA);
+            return View(Noticias_Seplan);
+
+        }
+        // GET: EliminarNoticias
+        public ActionResult EliminarNoticiasAdmin(int? id)
+        {
+            //Asegurar que a esta vista solo entren aquellos usuarios con rol 1=Capturista 
+            int idUsuario = Convert.ToInt32(Session["IDUSUARIO"]);
+            byte? rol = null;
+            //linea para validar que no entre a los controladores hasta que detecte una autenticación
+            if (idUsuario == 0)
+            {
+                Response.Redirect("~/Login/Iniciar");
+                rol = null;
+            }
+            //en caso de que si detecte asignar el valor de rol y dar seguridad
+            else
+            {
+                rol = db.USUARIO.Where(s => s.IDUSUARIO == idUsuario).FirstOrDefault().ROL;
+                if (rol != 2) // 2 = Administrador
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+                else
+                {
+                    if (id == null)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+                    NOTICIAS_SEPLAN nOTICIAS_SEPLAN = db.NOTICIAS.Find(id);
+                    if (nOTICIAS_SEPLAN == null)
+                    {
+                        return HttpNotFound();
+                    }
+                    return View(nOTICIAS_SEPLAN);
+                }
+            }
+            return null;
+        }
+
+        // POST: SalidaPersonas/Delete/5
+        [HttpPost, ActionName("EliminarNoticiasAdmin")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EliminarNoticiasAdmin(int id)
+        {
+            NOTICIAS_SEPLAN nOTICIAS_SEPLAN = db.NOTICIAS.Find(id);
+            if (ModelState.IsValid)
+            {
+                //el valor I significa inactivo
+                nOTICIAS_SEPLAN.ESTATUS = "I";
+                db.Entry(nOTICIAS_SEPLAN).Property(x => x.ESTATUS).IsModified = true;
+                db.Entry(nOTICIAS_SEPLAN).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ListaNoticiasAdmin", new { id = nOTICIAS_SEPLAN.IDNOTICIA });
+            }
+            return View(nOTICIAS_SEPLAN);
+        }
+
+        //Seccion de fotos
+        public ActionResult ListaFotosAdmin(int? idNoticia)
+        {
+            //Asegurar que a esta vista solo entren aquellos usuarios con rol 1=Capturista 
+            int idUsuario = Convert.ToInt32(Session["IDUSUARIO"]);
+            byte? rol = null;
+            //linea para validar que no entre a los controladores hasta que detecte una autenticación
+            if (idUsuario == 0)
+            {
+                Response.Redirect("~/Login/Iniciar");
+                rol = null;
+            }
+            //en caso de que si detecte asignar el valor de rol y dar seguridad
+            else
+            {
+                rol = db.USUARIO.Where(s => s.IDUSUARIO == idUsuario).FirstOrDefault().ROL;
+                if (rol != 2) // 2 = Administrador
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+                else
+                {
+                    //encontrar por id 
+                    if (idNoticia == null)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+                    //mostrar lista referente a idnoticias
+                    ViewBag.idNoticia = idNoticia;
+                    return View(db.FOTOS.Where(p => p.IDNOTICIA == idNoticia.Value).ToList());
+                }
+            }
+            return null;
+        }
+        // GET: FotosTesting/Edit/5
+        public ActionResult EditarFotoAdmin(int? id)
+        {
+            //Asegurar que a esta vista solo entren aquellos usuarios con rol 2=Administrador 
+            int idUsuario = Convert.ToInt32(Session["IDUSUARIO"]);
+            byte? rol = null;
+            //linea para validar que no entre a los controladores hasta que detecte una autenticación
+            if (idUsuario == 0)
+            {
+                Response.Redirect("~/Login/Iniciar");
+                rol = null;
+            }
+            //en caso de que si detecte asignar el valor de rol y dar seguridad
+            else
+            {
+                rol = db.USUARIO.Where(s => s.IDUSUARIO == idUsuario).FirstOrDefault().ROL;
+                if (rol != 2) // 2 = Administrador
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+                else
+                {
+                    if (id == null)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+                    FOTOS fOTOS = db.FOTOS.Find(id);
+                    if (fOTOS == null)
+                    {
+                        return HttpNotFound();
+                    }
+                    ViewBag.IDNOTICIA = new SelectList(db.NOTICIAS, "IDNOTICIA", "TITULO", fOTOS.IDNOTICIA);
+                    return View(fOTOS);
+                }
+            }
+            return null;
+        }
+
+        // POST: FotosTesting/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditarFotoAdmin([Bind(Include = "IDNOTICIA,IDFOTO")] FOTOS fOTOS, HttpPostedFileBase FOTOGRAFIA)
+        {
+            if (FOTOGRAFIA != null && FOTOGRAFIA.ContentLength > 0)
+            {
+                byte[] imageData = null;
+                using (var binaryMunicipio = new BinaryReader(FOTOGRAFIA.InputStream))
+                {
+                    imageData = binaryMunicipio.ReadBytes(FOTOGRAFIA.ContentLength);
+                }
+                fOTOS.FOTOGRAFIA = imageData;
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(fOTOS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ListaFotosAdmin", new { idNoticia = fOTOS.IDNOTICIA });
+            }
+            ViewBag.IDNOTICIA = new SelectList(db.NOTICIAS, "IDNOTICIA", "TITULO", fOTOS.IDNOTICIA);
+            return View(fOTOS);
+        }
+
         //Modulo de usuarios
         public ActionResult ListaUsuarios(string filtrado, string currentFilter, string busqueda, int? page)
         {
@@ -190,33 +460,6 @@ namespace siteSPPP.Controllers
             }
             return View(uSUARIO);
         }
-
-        // GET: USUARIOs/Delete/5
-        public ActionResult EliminarUsuario(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            USUARIO uSUARIO = db.USUARIO.Find(id);
-            if (uSUARIO == null)
-            {
-                return HttpNotFound();
-            }
-            return View(uSUARIO);
-        }
-
-        // POST: USUARIOs/Delete/5
-        [HttpPost, ActionName("EliminarUsuario")]
-        [ValidateAntiForgeryToken]
-        public ActionResult EliminarUsuarioConfirmed(int id)
-        {
-            USUARIO uSUARIO = db.USUARIO.Find(id);
-            db.USUARIO.Remove(uSUARIO);
-            db.SaveChanges();
-            return RedirectToAction("ListaUsuarios");
-        }
-
         //Modulo de Funcionarios Públicos
         public ActionResult ListaFuncionarios(string filtrado, string currentFilter, string busqueda, int? page)
         {
